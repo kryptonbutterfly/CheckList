f48863df17c2b71b1f17308c285e67e95070388c
tinycodecrank <tinycodecrank@gmail.com>
2022-07-29 17:56:24 +0200

    add support for multiline tasks, auto show keyboard

--- app/src/main/java/de/tinycodecrank/checklist/CreateTaskActivity.kt
+++ app/src/main/java/de/tinycodecrank/checklist/CreateTaskActivity.kt
@@ -1,9 +1,11 @@
 package de.tinycodecrank.checklist
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
@@ -18,6 +20,11 @@ class CreateTaskActivity : AppCompatActivity() {
         intent.getStringExtra(TASK_DESCRIPTION)?.let { description ->
             taskDescription.setText(description)
         }
+        taskDescription.requestFocus()
+        taskDescription.postDelayed( {
+            (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .showSoftInput(taskDescription, InputMethodManager.SHOW_FORCED)
+        }, 0)
     }
 
     fun onApply(view: View) {
